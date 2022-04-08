464baeb76143f5d05cc00e7e9fc209eb801ca0ba
mohan.k <mohan.k@AXDESK53>
2022-04-08 10:28:24 +0530

    Add project files.

---
+++ CURD/Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace CURD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtrn = new System.Windows.Forms.TextBox();
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.txtage = new System.Windows.Forms.TextBox();
+            this.txtcourse = new System.Windows.Forms.TextBox();
+            this.btnclear = new System.Windows.Forms.Button();
+            this.btnsave = new System.Windows.Forms.Button();
+            this.btndelete = new System.Windows.Forms.Button();
+            this.btnsearch = new System.Windows.Forms.Button();
+            this.btnupdate = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.studentDataSet = new CURD.StudentDataSet();
+            this.studentdetail1BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.student_detail1TableAdapter = new CURD.StudentDataSetTableAdapters.student_detail1TableAdapter();
+            this.rollnoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.snameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ageDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.courseDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.studentDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.studentdetail1BindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(106, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Rool Number";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(106, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Student Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(106, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Age";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(106, 175);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Course";
+            // 
+            // txtrn
+            // 
+            this.txtrn.Location = new System.Drawing.Point(253, 33);
+            this.txtrn.Name = "txtrn";
+            this.txtrn.Size = new System.Drawing.Size(100, 20);
+            this.txtrn.TabIndex = 4;
+            this.txtrn.TextChanged += new System.EventHandler(this.txtrn_TextChanged);
+            // 
+            // txtname
+            // 
+            this.txtname.Location = new System.Drawing.Point(253, 87);
+            this.txtname.Name = "txtname";
+            this.txtname.Size = new System.Drawing.Size(100, 20);
+            this.txtname.TabIndex = 5;
+            // 
+            // txtage
+            // 
+            this.txtage.Location = new System.Drawing.Point(253, 128);
+            this.txtage.Name = "txtage";
+            this.txtage.Size = new System.Drawing.Size(100, 20);
+            this.txtage.TabIndex = 6;
+            // 
+            // txtcourse
+            // 
+            this.txtcourse.Location = new System.Drawing.Point(253, 168);
+            this.txtcourse.Name = "txtcourse";
+            this.txtcourse.Size = new System.Drawing.Size(100, 20);
+            this.txtcourse.TabIndex = 7;
+            // 
+            // btnclear
+            // 
+            this.btnclear.Location = new System.Drawing.Point(30, 230);
+            this.btnclear.Name = "btnclear";
+            this.btnclear.Size = new System.Drawing.Size(75, 23);
+            this.btnclear.TabIndex = 8;
+            this.btnclear.Text = "Clear";
+            this.btnclear.UseVisualStyleBackColor = true;
+            this.btnclear.Click += new System.EventHandler(this.btnclear_Click);
+            // 
+            // btnsave
+            // 
+            this.btnsave.Location = new System.Drawing.Point(138, 230);
+            this.btnsave.Name = "btnsave";
+            this.btnsave.Size = new System.Drawing.Size(75, 23);
+            this.btnsave.TabIndex = 9;
+            this.btnsave.Text = "Save";
+            this.btnsave.UseVisualStyleBackColor = true;
+            this.btnsave.Click += new System.EventHandler(this.btnsave_Click);
+            // 
+            // btndelete
+            // 
+            this.btndelete.Location = new System.Drawing.Point(253, 230);
+            this.btndelete.Name = "btndelete";
+            this.btndelete.Size = new System.Drawing.Size(75, 23);
+            this.btndelete.TabIndex = 10;
+            this.btndelete.Text = "Delete";
+            this.btndelete.UseVisualStyleBackColor = true;
+            this.btndelete.Click += new System.EventHandler(this.btndelete_Click);
+            // 
+            // btnsearch
+            // 
+            this.btnsearch.Location = new System.Drawing.Point(349, 230);
+            this.btnsearch.Name = "btnsearch";
+            this.btnsearch.Size = new System.Drawing.Size(75, 23);
+            this.btnsearch.TabIndex = 11;
+            this.btnsearch.Text = "Search";
+            this.btnsearch.UseVisualStyleBackColor = true;
+            this.btnsearch.Click += new System.EventHandler(this.btnsearch_Click);
+            // 
+            // btnupdate
+            // 
+            this.btnupdate.Location = new System.Drawing.Point(444, 230);
+            this.btnupdate.Name = "btnupdate";
+            this.btnupdate.Size = new System.Drawing.Size(75, 23);
+            this.btnupdate.TabIndex = 12;
+            this.btnupdate.Text = "Update";
+            this.btnupdate.UseVisualStyleBackColor = true;
+            this.btnupdate.Click += new System.EventHandler(this.btnupdate_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.rollnoDataGridViewTextBoxColumn,
+            this.snameDataGridViewTextBoxColumn,
+            this.ageDataGridViewTextBoxColumn,
+            this.courseDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.studentdetail1BindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(551, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(474, 263);
+            this.dataGridView1.TabIndex = 13;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // studentDataSet
+            // 
+            this.studentDataSet.DataSetName = "StudentDataSet";
+            this.studentDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // studentdetail1BindingSource
+            // 
+            this.studentdetail1BindingSource.DataMember = "student_detail1";
+            this.studentdetail1BindingSource.DataSource = this.studentDataSet;
+            // 
+            // student_detail1TableAdapter
+            // 
+            this.student_detail1TableAdapter.ClearBeforeFill = true;
+            // 
+            // rollnoDataGridViewTextBoxColumn
+            // 
+            this.rollnoDataGridViewTextBoxColumn.DataPropertyName = "roll_no";
+            this.rollnoDataGridViewTextBoxColumn.HeaderText = "roll_no";
+            this.rollnoDataGridViewTextBoxColumn.Name = "rollnoDataGridViewTextBoxColumn";
+            // 
+            // snameDataGridViewTextBoxColumn
+            // 
+            this.snameDataGridViewTextBoxColumn.DataPropertyName = "s_name";
+            this.snameDataGridViewTextBoxColumn.HeaderText = "s_name";
+            this.snameDataGridViewTextBoxColumn.Name = "snameDataGridViewTextBoxColumn";
+            // 
+            // ageDataGridViewTextBoxColumn
+            // 
+            this.ageDataGridViewTextBoxColumn.DataPropertyName = "age";
+            this.ageDataGridViewTextBoxColumn.HeaderText = "age";
+            this.ageDataGridViewTextBoxColumn.Name = "ageDataGridViewTextBoxColumn";
+            // 
+            // courseDataGridViewTextBoxColumn
+            // 
+            this.courseDataGridViewTextBoxColumn.DataPropertyName = "course";
+            this.courseDataGridViewTextBoxColumn.HeaderText = "course";
+            this.courseDataGridViewTextBoxColumn.Name = "courseDataGridViewTextBoxColumn";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1051, 479);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnupdate);
+            this.Controls.Add(this.btnsearch);
+            this.Controls.Add(this.btndelete);
+            this.Controls.Add(this.btnsave);
+            this.Controls.Add(this.btnclear);
+            this.Controls.Add(this.txtcourse);
+            this.Controls.Add(this.txtage);
+            this.Controls.Add(this.txtname);
+            this.Controls.Add(this.txtrn);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.studentDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.studentdetail1BindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtrn;
+        private System.Windows.Forms.TextBox txtname;
+        private System.Windows.Forms.TextBox txtage;
+        private System.Windows.Forms.TextBox txtcourse;
+        private System.Windows.Forms.Button btnclear;
+        private System.Windows.Forms.Button btnsave;
+        private System.Windows.Forms.Button btndelete;
+        private System.Windows.Forms.Button btnsearch;
+        private System.Windows.Forms.Button btnupdate;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private StudentDataSet studentDataSet;
+        private System.Windows.Forms.BindingSource studentdetail1BindingSource;
+        private StudentDataSetTableAdapters.student_detail1TableAdapter student_detail1TableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn rollnoDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn snameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ageDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn courseDataGridViewTextBoxColumn;
+    }
+}
+
---
+++ CURD/Form1.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace CURD
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conn;
+        SqlCommand comm;
+        SqlDataReader dreader;
+
+        DataSet dset;
+        SqlDataAdapter dadapter;
+
+        string connstring = "Data Source=AXDESK53\\SQLEXPRESS;Initial Catalog = Student; User ID = sa; Password=Axcend123";  
+
+        
+
+        //SqlConnection conn = new SqlConnection(@"Data Source=AXDESK53\\SQLEXPRESS;Initial Catalog=Student;User ID=sa;Password=Axcend123");
+
+
+        //private string connstring;
+        //SqlConnection conn = new SqlConnection();
+        //conn.ConnectionString = ConfigurationSettings.AppSettings["databasepath"];
+
+        //conn.Open();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+
+
+        private void btnsave_Click(object sender, EventArgs e)
+        {
+            conn = new SqlConnection(connstring);
+            conn.Open();
+            comm = new SqlCommand("insert into student_detail1 values(" + txtrn.Text + ",'" + txtname.Text + "'," + txtage.Text + ",'" + txtcourse.Text + "')", conn);
+            try
+            {
+                comm.ExecuteNonQuery();
+                MessageBox.Show("Saved...");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Not Saved");
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+           
+            {
+                dadapter = new SqlDataAdapter("select * from student_detail1", connstring);
+                dset = new System.Data.DataSet();
+                dadapter.Fill(dset);
+                dataGridView1.DataSource = dset.Tables[0].DefaultView;
+            }
+        }
+
+        private void btnclear_Click(object sender, EventArgs e)
+        {
+            {
+                txtage.Clear();
+                txtcourse.Clear();
+                txtname.Clear();
+                txtrn.Clear();
+                txtrn.Focus();
+            }
+
+            //{
+            //    dadapter = new SqlDataAdapter("select * from student_detail1", connstring);
+            //    dset = new System.Data.DataSet();
+            //    dadapter.Fill(dset);
+            //    dataGridView1.DataSource = dset.Tables[0].DefaultView;
+            //}
+        }
+
+        private void btndelete_Click(object sender, EventArgs e)
+        {
+            {
+                conn = new SqlConnection(connstring);
+                conn.Open();
+                comm = new SqlCommand("delete from student_detail1 where roll_no = " + txtrn.Text + " ", conn);
+                try
+                {
+                    comm.ExecuteNonQuery();
+                    MessageBox.Show("Deleted...");
+                    txtage.Clear();
+                    txtcourse.Clear();
+                    txtname.Clear();
+                    txtrn.Clear();
+                    txtrn.Focus();
+                }
+                catch (Exception x)
+                {
+                    MessageBox.Show(" Not Deleted" + x.Message);
+                }
+                finally
+                {
+                    conn.Close();
+                }
+
+                {
+                    dadapter = new SqlDataAdapter("select * from student_detail1", connstring);
+                    dset = new System.Data.DataSet();
+                    dadapter.Fill(dset);
+                    dataGridView1.DataSource = dset.Tables[0].DefaultView;
+                }
+            }
+        }
+
+        private void btnsearch_Click(object sender, EventArgs e)
+        {
+            {
+                conn = new SqlConnection(connstring);
+                conn.Open();
+                comm = new SqlCommand("select * from student_detail1 where roll_no = " + txtrn.Text + " ", conn);
+                try
+                {
+                    dreader = comm.ExecuteReader();
+                    if (dreader.Read())
+                    {
+                        txtname.Text = dreader[1].ToString();
+                        txtage.Text = dreader[2].ToString();
+                        txtcourse.Text = dreader[3].ToString();
+                    }
+                    else
+                    {
+                        MessageBox.Show(" No Record");
+                    }
+                    dreader.Close();
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show(" No Record");
+                }
+                finally
+                {
+                    conn.Close();
+                }
+
+                {
+                    dadapter = new SqlDataAdapter("select * from student_detail1", connstring);
+                    dset = new System.Data.DataSet();
+                    dadapter.Fill(dset);
+                    dataGridView1.DataSource = dset.Tables[0].DefaultView;
+                }
+            }
+        }
+
+        private void btnupdate_Click(object sender, EventArgs e)
+        {
+            {
+                conn = new SqlConnection(connstring);
+                conn.Open();
+                comm = new SqlCommand("update student_detail1 set s_name= '" + txtname.Text + "', age= " + txtage.Text + " , course=' " + txtcourse.Text + "' where roll_no = "+txtrn.Text+" ", conn);       
+
+
+               
+            try
+                {
+                    comm.ExecuteNonQuery();
+                    MessageBox.Show("Updated..");
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show(" Not Updated");
+                }
+                finally
+                {
+                    conn.Close();
+                }
+
+                {
+                    dadapter = new SqlDataAdapter("select * from student_detail1", connstring);
+                    dset = new System.Data.DataSet();
+                    dadapter.Fill(dset);
+                    dataGridView1.DataSource = dset.Tables[0].DefaultView;
+                }
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'studentDataSet.student_detail1' table. You can move, or remove it, as needed.
+            this.student_detail1TableAdapter.Fill(this.studentDataSet.student_detail1);
+            txtrn.Focus();
+
+          
+        }
+
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
+                txtrn.Text = row.Cells[0].Value.ToString();
+                txtname.Text = row.Cells[1].Value.ToString();
+                txtage.Text = row.Cells[2].Value.ToString();
+                txtcourse.Text = row.Cells[3].Value.ToString();
+            }
+        }
+
+
+
+
+
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtrn_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+}
